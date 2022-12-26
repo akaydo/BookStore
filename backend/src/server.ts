0553875957cbe3282ece96f29bb1f9cb47174de9
akaydo <tecno4kaa@mail.ru>
2022-12-26 22:19:33 +0300

    Deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import path from "path";
 import express from "express";
 import cors from "cors";
 import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
 import { dbConnect } from "./configs/database.config";
 import orderRouter from "./routers/order.router";
+import mongoose from "mongoose";
 dbConnect();
 
 const app = express();
+
+mongoose.set("strictQuery", true);
 app.use(express.json());
 app.use(
   cors({
@@ -21,6 +25,11 @@ app.use(
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
